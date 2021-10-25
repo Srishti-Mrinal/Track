23809b32ad54f33c9dba21c859d7d9ba45215f34
Srishti Mrinal <srishti062mca21@igdtuw.ac.in>
2021-10-25 12:11:59 +0530

    ifElse

---
+++ DSA/Day1/code1.java
@@ -0,0 +1,34 @@
+package Day1;
+import java.util.*;
+
+//https://www.hackerrank.com/challenges/java-if-else/problem?isFullScreen=true
+
+class Solution {
+    //public Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(n % 2 == 0)
+        {
+            if(n >= 2 && n <= 5)
+            {
+                System.out.println("Not Weird");
+            }
+            else if(n >= 6 && n <= 20)
+            {
+                System.out.println("Weird");
+            }
+            else if(n > 20)
+            {
+                System.out.println("Not Weird");                
+            }    
+        }
+        else
+        {
+            System.out.println("Weird");
+        }
+        scanner.close();
+    }
+}
